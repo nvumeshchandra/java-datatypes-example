7fb7c229d24b2b71e41ff4bd3a9074a3a2c6f81c
nvumeshchandra <nvenkataumeshchandra@gmail.com>
2025-07-16 17:42:42 +0530

    Add files via upload
    java code to perform various datatypes

---
+++ DataTypesExample.java
@@ -0,0 +1,41 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        // byte: 8-bit signed integer
+        byte b = 100;
+
+        // short: 16-bit signed integer
+        short  s= 10000;
+
+        // int: 32-bit signed integer
+        int i = 100000;
+
+        // long: 64-bit signed integer
+        long l = 10000000000L;
+
+        // float: 32-bit floating point
+        float f = 3.14f;
+
+        // double: 64-bit floating point
+        double d = 3.141592653589793;
+
+        // char: single 16-bit Unicode character
+        char c = 'A';
+
+        // boolean: true or false
+        Boolean bo= true;
+
+        // String: sequence of characters (not a primitive type)
+        String st = "Hello, Java!";
+
+        // Print all values
+        System.out.println("byte value: " + b);
+        System.out.println("short value: " + s);
+        System.out.println("int value: " + i);
+        System.out.println("long value: " + l);
+        System.out.println("float value: " + f);
+        System.out.println("double value: " + d);
+        System.out.println("char value: " + c);
+        System.out.println("boolean value: " + bo);
+        System.out.println("String value: " + st);
+    }
+}
